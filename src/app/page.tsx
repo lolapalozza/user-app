c3c302239ca583682f165dd4ff7327c9dee5a59d
Fyodor Khruschov <fkhruschov@roketus.com>
2024-07-08 22:08:37 +0300

    fix menu gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul className="flex gap-20">
+      <ul className="flex gap-10">
         <li className="flex flex-col items-center">
           <Image
             src="/icons/envelope.png"
