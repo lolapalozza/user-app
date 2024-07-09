f750ae15bd9c360ac9a6d887631351667a343515
Fyodor Khruschov <fkhruschov@roketus.com>
2024-07-09 15:49:21 +0300

    balance page

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import Link from "next/link";
+import QRCode from "react-qr-code";
 
 export default function Balance() {
 
+  const balance = 0
+  const address = "TRDGtt4EL9cvGkRqCpnNoqNiQHojVdPHM2"
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
+    <main className="flex min-h-screen flex-col items-center p-24 relative">
 
       <div className="absolute left-0 ml-20">
         <span>
@@ -16,6 +20,15 @@ export default function Balance() {
       <h1 className="mb-40">
         Balance
       </h1>
+
+      <h2 className="mb-10">Your current Balance is {balance} USD</h2>
+
+      <p>Deposit with USDT (TRC-20)</p>
+      <p className="mb-20">Your balance will be updated automatically</p>
+
+      {address}
+      <QRCode value={address} />
+
     </main>
   );
 }
