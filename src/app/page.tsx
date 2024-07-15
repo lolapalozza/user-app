fde9c2acdae9914e8fe303068ce201984bbb1842
Fyodor Khruschov <fkhruschov@roketus.com>
2024-07-15 17:10:03 +0300

    try to get TG data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,32 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Script from "next/script";
 
 export default function Home() {
 
   const [userId, setUserId] = useState(null);
   const [username, setUsername] = useState(null);
-  const [tg, setTg] = useState({})
+  const [tg, setTg] = useState("")
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(window.Telegram){
+        const a = JSON.stringify(window.Telegram.WebApp)
+        setTg(a)
+        clearInterval(interval)
+      }
+    }, 5000)
+
+  },[])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <Script src="https://telegram.org/js/telegram-web-app.js"/>
+
+      {tg}
 
       <ul className="flex gap-10">
         <li className="flex flex-col items-center">
