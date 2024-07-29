33d15bded25b929ac15b1a693e562fb9261ef711
Fyodor Khruschov <fkhruschov@roketus.com>
2024-07-29 23:01:06 +0300

    main button order inpost

--- src/app/inpost/cart/PlaceOrderButton.tsx
+++ src/app/inpost/cart/PlaceOrderButton.tsx
@@ -5,9 +5,10 @@ export const PlaceOrderButton = ({onSubmit, show}) => {
   useEffect(() => {
     if(show){
       window.Telegram.WebApp.MainButton.setText("Place Order");
-      window.Telegram.WebApp.MainButton.onClick(onSubmit);
       window.Telegram.WebApp.MainButton.show();
+      window.Telegram.WebApp.onEvent('mainButtonClicked', onSubmit)
       return () => {
+        window.Telegram.WebApp.offEvent('mainButtonClicked', onSubmit)
         window.Telegram.WebApp.MainButton.hide();
       }
     }else{
