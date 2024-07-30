f07a52f905ac7c1a8ebecd77a2b414ce9e694efa
Fyodor Khruschov <fkhruschov@roketus.com>
2024-07-30 12:09:18 +0300

    display drop data and photios

--- src/app/orders/drops/[dropId]/page.tsx
+++ src/app/orders/drops/[dropId]/page.tsx
@@ -1,13 +1,20 @@
 'use client'
 
 import {BackButton} from "@/shared/BackButton";
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {getBoughtDrops, getDropById} from "@/app/orders/api";
 import {formatDate} from "@/app/orders/formatDate";
 
 export default function OrdersDrop({params}) {
 
   const [drop, setDrop] = useState({})
+
+  const photos = useMemo(() => {
+    if(!drop || !drop.photos) return []
+    return drop.photos.split(",")
+  }, [drop])
+
+  console.log(photos)
 
   useEffect(() => {
     getDropById(params.dropId).then((setDrop))
@@ -19,14 +26,15 @@ export default function OrdersDrop({params}) {
     </h1>
 
     {drop.id && <>
-      {drop.district.city.city}<br/>
-      {drop.district.district}<br/>
-      {drop.packages.product.short_description}<br/>
-      {drop.packages.product.short_description}<br/>
-      {drop.packages.quantity}<br/>
-      {drop.photos}<br/>
-      {drop.comment}<br/>
-      {formatDate(drop.sold_at)}<br/>
+      <div>{drop.packages.product.short_description} - {drop.packages.quantity} {drop.packages.product.measure}</div>
+      <div className="mb-2">{drop.district.city.city}, {drop.district.district}</div>
+
+      {
+        photos.map(photo => <img key={photo} className="mb-2" src={`${process.env.NEXT_PUBLIC_API_URL}/photos/${photo}`} />)
+      }
+
+      <div className="mb-5">{drop.comment}</div>
+      <div className="text-sm">Purchase Date: {formatDate(drop.sold_at)}</div>
     </>}
   </>
   );
