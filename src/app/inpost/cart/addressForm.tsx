9e0d42870f0a28323938bc2de06610036d459db1
Fyodor Khruschov <fkhruschov@roketus.com>
2024-07-30 14:54:56 +0300

    fix place order button

--- src/app/inpost/cart/addressForm.tsx
+++ src/app/inpost/cart/addressForm.tsx
@@ -18,7 +18,9 @@ export const AddressForm = ({createInpost, showPlaceOrderButton}) => {
   const inputValidClasses = "w-full h-9 mb-2 p-1 rounded text-black"
 
   const onSubmit = async(e) => {
-    // e.preventDefault()
+    if(process.env.NEXT_PUBLIC_ENV === 'local'){
+      e.preventDefault()
+    }
 
     const _formErrors = {}
     if(!email){
