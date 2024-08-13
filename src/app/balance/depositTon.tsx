693ba5d78bbb45f3c9d2b6c81f509eb3ab003fd2
Fyodor Khruschov <fkhruschov@roketus.com>
2024-08-13 15:50:31 +0300

    TON payment added

--- src/app/balance/depositTon.tsx
+++ src/app/balance/depositTon.tsx
@@ -1,7 +1,8 @@
 import {TonConnectButton, useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
 import {Loading} from "@/shared/Loading";
 import {useState} from "react";
-import { Address } from "@ton/core";
+
+const address = 'UQBS8sti9dUFpJ4oxf1oFrt58Vs_NuUJ4G4APe3_cEHtqYJM'
 
 export const DepositTon = () => {
   const wallet = useTonWallet();
@@ -10,30 +11,32 @@ export const DepositTon = () => {
   const [amount, setAmount] = useState(0)
   const [isLoading, setIsLoading] = useState(false)
 
-  const address = Address.parse('UQBS8sti9dUFpJ4oxf1oFrt58Vs_NuUJ4G4APe3_cEHtqYJM');
-
   const sendTransaction = () => {
     tonConnectUI.sendTransaction({
       validUntil: Math.floor(new Date() / 1000) + 360,
       messages: [
         {
-          address: address.toRawString(),
-          amount: "159713000" //Toncoin in nanotons
+          address: address,
+          amount: (amount * 1000000000).toString() //Toncoin in nanotons
         }
       ]
+    }).catch((e) => {
+      console.log(e)
     })
   }
 
   return <div>
     <TonConnectButton/>
-
-    <h3>Enter amount in TON</h3>
-    <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number"
-           className="bg-transparent text-5xl text-center mb-5 w-40 outline-none" autoFocus/>
-    <div className="flex flex-col items-center gap-2 justify-center">
-      <button className="border-2 border-white rounded p-2" onClick={sendTransaction}>Deposit</button>
-      {isLoading && <Loading/>}
-    </div>
-
+    {
+      wallet?.name && <div className="mt-5">
+        <h3>Enter amount in TON</h3>
+        <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number"
+               className="bg-transparent text-5xl text-center mb-5 w-40 outline-none" autoFocus/>
+        <div className="flex flex-col items-center gap-2 justify-center">
+          <button className="border-2 border-white rounded p-2" onClick={sendTransaction}>Deposit</button>
+          {isLoading && <Loading/>}
+        </div>
+      </div>
+    }
   </div>
 }
