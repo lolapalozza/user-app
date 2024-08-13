a8684fe9cda3cbfdb853103de54e095c48850f1b
Fyodor Khruschov <fkhruschov@roketus.com>
2024-08-13 14:17:00 +0300

    WIP ton connect

--- src/app/balance/depositTon.tsx
+++ src/app/balance/depositTon.tsx
@@ -1,8 +1,19 @@
-import {TonConnectButton} from "@tonconnect/ui-react";
+import {TonConnectButton, useTonConnectUI, useTonWallet} from "@tonconnect/ui-react";
 
 export const DepositTon = () => {
+  const wallet = useTonWallet();
+  const [tonConnectUI, setOptions] = useTonConnectUI();
+
   return <div>
     Soon ..
     <TonConnectButton />
+    {
+      wallet && (
+      <div>
+        <span>Connected wallet: {wallet.name}</span>
+        <span>Device: {wallet.device.appName}</span>
+      </div>
+      )
+    }
   </div>
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <Auth>
-            {children}
-          </Auth>
+        <Auth>
+          {children}
+        </Auth>
       </body>
     </html>
   );
