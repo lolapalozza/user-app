b53c81a8f1aab9c2db3e52e6d3403f9321432c39
Fyodor Khruschov <fkhruschov@roketus.com>
2024-08-19 08:50:20 +0300

    drop loading

--- src/app/drops/steps/SelectDrop.tsx
+++ src/app/drops/steps/SelectDrop.tsx
@@ -1,12 +1,16 @@
 import {useState} from "react";
 import {getDropsToBuy} from "@/app/drops/api";
+import {Loading} from "@/shared/Loading";
 
 export const SelectDrop = ({selection, setSelection}) => {
   const [drops, setDrops] = useState([])
+  const [dropsLoading, setDropsLoading] = useState(false)
 
   useState(() => {
+    setDropsLoading(true)
     getDropsToBuy(selection.city.id, selection.product.id).then((_drops) => {
       setDrops(_drops)
+      setDropsLoading(false)
     })
   },[])
 
@@ -20,13 +24,18 @@ export const SelectDrop = ({selection, setSelection}) => {
   return <div>
     <h2>Select Drop:</h2>
 
-    <ul className="mt-10 flex gap-1 flex-wrap">
-      {
-        drops.map((drop, index) => <li key={index}>
-          <button className="border-2 border-white p-3 rounded" onClick={() => onDropSelected(drop)}>{selection.product.product} {drop.amount}{drop.unit} - {drop.district.title} - {drop.price} PLN</button>
-        </li>)
-      }
-    </ul>
+    {
+      dropsLoading ? <div className="text-center"><Loading/></div> : <ul className="mt-10 flex gap-1 flex-wrap">
+        {
+          drops.map((drop, index) => <li key={index}>
+            <button className="border-2 border-white p-3 rounded"
+                    onClick={() => onDropSelected(drop)}>{selection.product.product} {drop.amount}{drop.unit} - {drop.district.title} - {drop.price} PLN
+            </button>
+          </li>)
+        }
+      </ul>
+    }
+
 
   </div>
 }
